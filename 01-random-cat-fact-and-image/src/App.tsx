2f643967c15c9cb6d5955f6ba9b62f45a420c4a1
Toni <cerdanigualtoni@gmail.com>
2024-06-15 19:32:33 +0200

    feat: get custom fact and set custom styles

--- 01-random-cat-fact-and-image/src/App.tsx
+++ 01-random-cat-fact-and-image/src/App.tsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
+
 function App() {
+  const [fact, setFact] = useState('test');
+  const url = 'https://catfact.ninja/fact';
+
+  const getCustomFact = () => {
+    fetch(url)
+      .then(res => res.json())
+      .then(({ fact }) => setFact(fact));
+  };
+
   return (
-    <main className="bg-gray-800 min-h-screen flex items-center justify-center ">
-      <button className="border-2 rounded-lg border-sky-300 text-sky-300 hover:bg-sky-300 hover:text-gray-800 p-2 mb-4">
-        Get custom fact
-      </button>
-      <div id="random-fact" className="hidden"></div>
-      <img className="hidden" src="" alt="Random cat image" />
+    <main
+      className="bg-gray-900 min-h-screen flex items-center justify-center"
+      style={{ backgroundColor: '#0c1016', color: '#7DD3F6' }}
+    >
+      <div className="flex flex-col items-center">
+        <button
+          onClick={getCustomFact}
+          className="border-2 rounded-lg border-primary-blue bg-dark-blue hover:bg-primary-blue text-primary-blue hover:text-dark-blue p-2 mb-4"
+        >
+          Get custom fact
+        </button>
+        <div id="random-fact" className={!fact ? 'hidden' : ''}>
+          {fact}
+        </div>
+        <img className="hidden" src="" alt="Random cat image" />
+      </div>
     </main>
   );
 }
