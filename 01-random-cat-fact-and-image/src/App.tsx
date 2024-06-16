ca10315654e9e0e09d69dd1fcb51cd3ec09d14ee
Toni <cerdanigualtoni@gmail.com>
2024-06-16 13:57:07 +0200

    feat: add header

--- 01-random-cat-fact-and-image/src/App.tsx
+++ 01-random-cat-fact-and-image/src/App.tsx
@@ -22,25 +22,30 @@ function App() {
   }, [fact]);
 
   return (
-    <main className="bg-dark-blue text-primary-blue min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center">
-        <button
-          onClick={getRandomFact}
-          className="border-2 rounded-lg border-primary-blue bg-dark-blue hover:bg-primary-blue text-primary-blue hover:text-dark-blue p-2 mb-4"
-        >
-          Get custom fact
-        </button>
-        <div id="random-fact" className={!fact ? 'hidden' : ''}>
-          {fact}
+    <>
+      <header className="text-center p-16">
+        <h1 className="text-5xl font-bold font-mono">Cat fact app</h1>
+      </header>
+      <main className="bg-dark-blue text-primary-blue min-h-screen flex justify-center font-mono">
+        <div className="flex flex-col items-center">
+          <button
+            onClick={getRandomFact}
+            className="border-2 rounded-lg border-primary-blue bg-dark-blue hover:bg-primary-blue text-primary-blue hover:text-dark-blue p-2 mb-4"
+          >
+            Get custom fact
+          </button>
+          <div id="random-fact" className={` p-5 ${!fact ? 'hidden' : ''}`}>
+            {fact}
+          </div>
+          <img
+            className={!imageUrl ? 'hidden' : ''}
+            src={imageUrl}
+            alt="Random cat image"
+            loading="lazy"
+          />
         </div>
-        <img
-          className={!imageUrl ? 'hidden' : ''}
-          src={imageUrl}
-          alt="Random cat image"
-          loading="lazy"
-        />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
